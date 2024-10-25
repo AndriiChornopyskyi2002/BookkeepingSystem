e15d9f4b60573a9da62dfa1d38c61e44703fd024
Andrii <andriichornopyskyi380@gmail.com>
2024-10-25 18:59:33 +0300

    add < burger-menu >

--- app/src/components/App/subComponents/Header.js
+++ app/src/components/App/subComponents/Header.js
@@ -1,27 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
-const Header = ({login, isLoggedIn}) => {
+const Header = ({ login, isLoggedIn }) => {
+    const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setMobileMenuOpen(!isMobileMenuOpen);
+    };
+
     return (
         <header className="bg-dark text-white text-center p-3">
-            <h1>Бібліотека</h1>
-            <nav>
-                <ul className="nav justify-content-center">
-                    <li className="nav-item">
-                        <Link className="nav-link text-white" to="/">Про нас</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link text-white"
-                              to="/profile">{isLoggedIn ? "Профіль" : "Увійти/Зареєструватись"}</Link>
-                    </li>
-                    {login === "admin" && isLoggedIn ? <li className="nav-item">
-                        <Link className="nav-link text-white" to="/users">Користувачі</Link>
-                    </li> : null}
-                    <li className="nav-item">
-                        <Link className="nav-link text-white" to="/books">Пошук книжок</Link>
-                    </li>
-                </ul>
-            </nav>
+            <div className="container">
+                <h1>Бібліотека</h1>
+
+                <Button
+                    onClick={toggleMobileMenu}
+                    className="d-md-none" // відображення тільки на мобільних пристроях
+                    aria-label="Toggle navigation"
+                >
+                    {isMobileMenuOpen ? <CloseIcon style={{color: 'white'}}/> : <MenuIcon style={{color: 'white'}}/>}
+
+                </Button>
+
+                <nav className={`${isMobileMenuOpen ? 'd-block' : 'd-none'} d-md-flex`}>
+                    <ul className="nav justify-content-center flex-column flex-md-row">
+                        <li className="nav-item">
+                            <Link className="nav-link text-white" to="/" onClick={() => setMobileMenuOpen(false)}>
+                                Про нас
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                className="nav-link text-white"
+                                to="/profile"
+                                onClick={() => setMobileMenuOpen(false)}
+                            >
+                                {isLoggedIn ? "Профіль" : "Увійти/Зареєструватись"}
+                            </Link>
+                        </li>
+                        {login === "admin" && isLoggedIn && (
+                            <li className="nav-item">
+                                <Link
+                                    className="nav-link text-white"
+                                    to="/users"
+                                    onClick={() => setMobileMenuOpen(false)}
+                                >
+                                    Користувачі
+                                </Link>
+                            </li>
+                        )}
+                        <li className="nav-item">
+                            <Link
+                                className="nav-link text-white"
+                                to="/books"
+                                onClick={() => setMobileMenuOpen(false)}
+                            >
+                                Пошук книжок
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 };
